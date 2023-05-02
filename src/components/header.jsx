8fc7571f8d89c09fae070844a3de065f61ddd2bb
gweryf <priyam2007sp@gmail.com>
2023-05-02 20:10:31 +0530

    added basic website and routing

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+const Header = ()=>{
+    return(
+        <nav>
+            <div className="header">.gweryf()</div>
+            <div className="navlinks"> 
+                <div>//about</div>
+                <div>//contact</div>
+            </div>
+        </nav>
+    )
+}
+export default Header
