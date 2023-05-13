c0e7cf644d12da29a8480c6f5214947b125fdbd7
gweryf <priyam2007sp@gmail.com>
2023-05-13 23:09:53 +0530

    fixed import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import RouteSwitch from './routeSwitch'
+import RouteSwitch from './routeSwitch.jsx'
 import "./styles/index.scss"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
