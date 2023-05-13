ce4086c42a0aa876b70ad4291a5a17cac97e339f
gweryf <priyam2007sp@gmail.com>
2023-05-13 21:21:45 +0530

    redesigned homepage

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,6 +1,9 @@
 import cv from "../assets/cv-builder.png"
 import todo from "../assets/todo-app.png"
 import etch from "../assets/etch-a-sketch.png"
+import mem from "../assets/memory-card.png"
+import wea from "../assets/weather-app.png"
+import bit from "../assets/bit-hunter.gif"
 import "../styles/project.scss"
 import "../styles/index.scss"
 const Projects = ()=>{
@@ -9,40 +12,68 @@ const Projects = ()=>{
             <p>Check out some of my</p>
             <h1>Projects</h1>
             <div className="container">
-                <div className="skilltable">
-                    
-                    <div className="projpic">
-                        <img src={cv} alt="screenshoit of the project cv-builder" />
-                    </div>
+                <div className="skilltable item1">
+                    <img src={cv} alt="screenshoit of the project cv-builder" />
                     <div className="details"> 
                         <h2>CV Builder</h2>
-                        <button><a href="https://cv-builder-lac.vercel.app/">Live Preview</a></button>
-                        <button><a href="https://github.com/gweryf/cv-builder">Github</a></button>
+                        <div>
+                            <button><a href="https://cv-builder-lac.vercel.app/">Live Preview</a></button>
+                            <button><a href="https://github.com/gweryf/cv-builder">Github</a></button>
+                        </div>
                     </div>
                 </div>
-                <div className="skilltable">
-                    <div className="projpic">
-                        <img src={todo} alt="screenshoit of the project cv-builder" />
-                    </div>
+                <div className="skilltable item2">
+                    <img src={todo} alt="screenshoit of the project cv-builder" />
                     <div className="details"> 
                         <h2>To-Do App</h2>
-                        <button><a href="https://gweryf.github.io/to-do-app/">Live Preview</a></button>
-                        <button><a href="https://github.com/gweryf/to-do-app">Github</a></button>
+                        <div>
+                            <button><a href="https://gweryf.github.io/to-do-app/">Live Preview</a></button>
+                            <button><a href="https://github.com/gweryf/to-do-app">Github</a></button>
+                        </div>
                     </div>
                 </div>
-                <div className="skilltable">
-                    
-                    <div className="projpic">
-                        <img src={etch} alt="screenshoit of the project cv-builder" />
-                    </div>
+                <div className="skilltable item3">
+                    <img src={etch} alt="screenshoit of the project cv-builder" />
                     <div className="details"> 
                         <h2>Etch-a-Sketch</h2>
-                        <button><a href="https://gweryf.github.io/etch-a-sketch/">Live Preview</a></button>
-                        <button><a href="https://github.com/gweryf/etch-a-sketch">Github</a></button>
+                        <div>
+                            <button><a href="https://gweryf.github.io/etch-a-sketch/">Live Preview</a></button>
+                            <button><a href="https://github.com/gweryf/etch-a-sketch">Github</a></button>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="container1"></div>
+            <div className="container">
+                <div className="skilltable item1">
+                    <img src={wea} alt="screenshoit of the project cv-builder" />
+                    <div className="details"> 
+                        <h2>Weather App</h2>
+                        <div>
+                            <button><a href="https://gweryf.github.io/weather-app/">Live Preview</a></button>
+                            <button><a href="https://github.com/gweryf/weather-app">Github</a></button>
+                        </div>
+                    </div>
+                </div>
+                <div className="skilltable item2">
+                    <img src={mem} alt="screenshoit of the project cv-builder" />
+                    <div className="details"> 
+                        <h2>Memory Card</h2>
+                        <div>
+                            <button><a href="https://memory-card-kappa.vercel.app/">Live Preview</a></button>
+                            <button><a href="https://github.com/gweryf/memory-card">Github</a></button>
+                        </div>
+                    </div>
+                </div>
+                <div className="skilltable item3">
+                    <img src={bit} alt="screenshoit of the project cv-builder" />
+                    <div className="details"> 
+                        <h2>Bit Hunter</h2>
+                        <div>
+                            <button><a href="https://github.com/gweryf/105_BigBitPirates_Hackover3.0">Github</a></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
