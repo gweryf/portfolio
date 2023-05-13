74274b8c8c27b50a9ea892d3d284c86352d552bc
gweryf <priyam2007sp@gmail.com>
2023-05-13 23:11:57 +0530

    oof

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import RouteSwitch from './routeSwitch.jsx'
 import "./styles/index.scss"
-
+import RouteSwitch from './routeSwitch'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouteSwitch />
